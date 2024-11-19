5e9b30805d03869fc34541d511f9b4fdc98204d3
Jeff <j3ffh95@gmail.com>
2024-11-18 23:25:10 -0500

    Added event listener to the form element

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 const list = document.querySelector<HTMLUListElement>("#list");
 const form = document.querySelector<HTMLFormElement>("#new-task-form");
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
+
+form?.addEventListener("submit", e => {
+  e.preventDefault();
+});
