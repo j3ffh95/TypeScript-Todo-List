6d400375183c224a58c0c54074a7f26f367add63
Jeff <j3ffh95@gmail.com>
2024-11-18 23:24:25 -0500

    Created the html list, form and input variables

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.querySelector<HTMLFormElement>("#new-task-form");
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
