aa6dfb790728491ff20b89a12f4ad1a9329b0293
Jeff <j3ffh95@gmail.com>
2024-11-18 23:26:52 -0500

    Checked to see if the input is empty or not

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,6 @@ const input = document.querySelector<HTMLInputElement>("#new-task-title");
 
 form?.addEventListener("submit", e => {
   e.preventDefault();
+
+  if (input?.value == "" || input?.value == null) return;
 });
